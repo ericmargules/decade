cf8ba1d832af07ca5c7e91d581ab3d47822ad283
ericmargules <eric.i.margules@gmail.com>
2016-06-14 23:51:42 -0500

    refine image carousel on product show

--- app/assets/javascripts/product.js
+++ app/assets/javascripts/product.js
@@ -3,7 +3,10 @@ $(document).ready(function(){
   	items: 1,
     loop:true,
     margin:10,
+    //nav:true,
     autoplay:false,
+    URLhashListener:true,
+    startPosition: 'URLHash'
     // autoplayTimeout:6000,
   });
 	// var owl = $('.owl-carousel');
