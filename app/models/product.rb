11e12b8ecfec60e7366af7ec9f68bc13606ddde5
ericmargules <eric.i.margules@gmail.com>
2016-07-07 22:23:36 -0500

    add second responsive snap point

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,10 @@
 class Product < ActiveRecord::Base
 
-  has_attached_file :main_image, styles: { large: "1000x1000>", medium: "500x500>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :main_image, styles: { large: "1250x1250>", medium: "750x750>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :main_image, content_type: /\Aimage\/.*\Z/
-  has_attached_file :interior_image, styles: { large: "1000x1000>", medium: "500x500>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :interior_image, styles: { large: "1250x1250>", medium: "750x750>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :interior_image, content_type: /\Aimage\/.*\Z/
-  has_attached_file :context_image, styles: { large: "1000x1000>", medium: "500x500>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :context_image, styles: { large: "1250x1250>", medium: "750x750>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :context_image, content_type: /\Aimage\/.*\Z/
 
 end
