008a40c7c60f3bb1bdd9af116b9728601fee29fa
Eric Margules <eric.i.margules@gmail.com>
2017-02-09 21:39:55 -0600

    try moving image creation before save of CP

--- app/models/custom_product.rb
+++ app/models/custom_product.rb
@@ -1,5 +1,5 @@
 class CustomProduct < ActiveRecord::Base
   has_attached_file :image, styles: { default: "667×500>", thumb: "133x100>" }, default_url: "/images/:style/missing.png"
-  # validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
-  validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  #validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
 end
