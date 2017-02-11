82a463c96478720d481af0a193b1a06cca0087dd
Eric Margules <eric.i.margules@gmail.com>
2017-02-10 22:01:18 -0600

    dummy URL for paperclip

--- app/models/custom_product.rb
+++ app/models/custom_product.rb
@@ -1,14 +1,11 @@
-require "uri"
+#require "uri"
 
 class CustomProduct < ActiveRecord::Base
   has_attached_file :image, styles: { default: "667×500>", thumb: "133x100>" }, default_url: "/images/:style/missing.png"
-  
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
   def image_from_url(url)
-  	if url.present?
-	  	self.image = URI.parse(url)
-  	end
+  	self.image = URI.parse(url)
   end
 
 end
